3a6e98c85aa5b102c0e802725e7039e88de88a0d
bhagirathpaliyal <bhagirathpaliyal@gmail.com>
2025-01-27 22:57:21 +0530

    fix:fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Payment from './components/Payment';
-import PaymentSuccess from './components/PaymentSuccess';
-
-function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    <Route path="/" element={<Payment />} />
-                    <Route path="/payment-success" element={<PaymentSuccess />} />
-                </Routes>
-            </div>
-        </Router>
-    );
-}
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import StripeContainer from "./components/Payment"; 
+import PaymentSuccess from "./components/PaymentSuccess";
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<StripeContainer />} />
+        <Route path="/payment-successful" element={<PaymentSuccess />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
